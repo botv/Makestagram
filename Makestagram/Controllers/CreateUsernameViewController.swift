c1ce335e9761e3e87f22e98b12d77d48eec078d5
botvinick <benbotvinick@gmail.com>
2018-07-09 13:50:32 -0700

    Integrate FirebaseDatabase for authentication with username

--- Makestagram/Controllers/CreateUsernameViewController.swift
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class CreateUsernameViewController : UIViewController {
     
-    @IBOutlet weak var createUsernameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
@@ -22,7 +24,15 @@ class CreateUsernameViewController : UIViewController {
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
-        print("Hi")
+        guard let firUser = Auth.auth().currentUser,
+            let username = usernameTextField.text,
+            !username.isEmpty else { return }
+        
+        UserService.create(firUser, username: username) { (user) in
+            guard let user = user else { return }
+            
+            print("Created new user: \(user.username)")
+        }
     }
     
     
