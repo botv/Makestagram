97076abd182bdede7da8389b765667a339e373cf
botvinick <benbotvinick@gmail.com>
2018-07-11 10:51:51 -0700

    Red table view cells for posts

--- Makestagram/Controllers/HomeViewController.swift
+++ Makestagram/Controllers/HomeViewController.swift
@@ -12,13 +12,28 @@ class HomeViewController: UIViewController {
     
     // Mark: - Properties
     
+    var posts = [Post]()
+    
     @IBOutlet weak var tableView: UITableView!
     
     // Mark: - VC Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        UserService.posts(for: User.current) { (posts) in
+            self.posts = posts
+            self.tableView.reloadData()
+        }
     }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return posts.count }
     
-    class HomeViewController
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
 }
