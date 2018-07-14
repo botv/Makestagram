4383f61ccda0d4e31c67add472713c9419b3d59f
botvinick <benbotvinick@gmail.com>
2018-07-14 15:09:49 -0700

    Update functions

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,6 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onCreate = functions.database.ref('/posts').onUpdate(function snapshot() {
+exports.onCreate = functions.database.ref('/posts').onUpdate(function (snapshot) {
   return console.log(snapshot)
 })
