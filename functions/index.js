13a500742b3da9b4233d5704652734738e451f7e
botvinick <benbotvinick@gmail.com>
2018-07-14 18:26:31 -0700

    Huge files

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,20 @@
-const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 const fr = require('face-recognition');
 
+firebase.initializeApp({
+	apiKey: "AIzaSyAFGJjHp3LeLeO2NwpPTqkFWCgi8tnH6kE",
+	authDomain: "makestagram-de700.firebaseapp.com",
+	databaseURL: "https://makestagram-de700.firebaseio.com",
+	projectId: "makestagram-de700",
+	storageBucket: "makestagram-de700.appspot.com",
+	messagingSenderId: "329513841758"
+});
+
+const database = firebase.database();
+
 const detector = fr.FaceDetector();
 const recognizer = fr.FaceRecognizer();
+
+database.ref('posts').on('child_added', function(snapshot) {
+	console.log(snapshot.ref)
+});
